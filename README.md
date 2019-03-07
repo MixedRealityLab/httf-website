d30e95a0afc5dbe1da9af56e3c0b9f33950a6174
Martin Porcheron <martin+github@porcheron.uk>
2019-03-07 14:17:00 +0000

    Initial commit of the theme

---
+++ README.md
@@ -0,0 +1,5 @@
+<h1 align="center">
+	HttF Website
+</h1>
+
+A theme for the Halfway to the Future 2019 website, built + copyrighted by Martin Porcheron for Bolt CMS (tested with Bolt CMS 3.6). HttF branding (c) Mixed Reality Lab at the University of Nottingham.
---
+++ httf2019/sass_on_change.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#inotifywait -e close_write,moved_to,create -m scss |
+fswatch scss |
+while read -r directory events filename; do
+	now=`date`
+	printf "%s: Running SASS..." "$now"
+	if sass scss/theme.scss &> css/theme.css; then
+		printf "done\n"
+		now=`date`
+		printf "%s: Uploading CSS...\n" "$now"
+		scp  -i ~/.ssh/httf css/theme.css halfway-admin@halfway.wp.horizon.ac.uk:/var/www/halfway/public/theme/httf2019/css
+	else
+		printf "failed!\n"
+	fi
+done
