c055e003c033d3e4e88b0b4cdf5bc85b23ed4be8
Martin Porcheron <martin+github@porcheron.uk>
2019-06-25 09:41:49 +0100

    Remove SQL file from theme

--- httf/convert.sql
+++
@@ -1,5 +0,0 @@
-update bolt_field_value set name='body' where name='blocks';
-update bolt_field_value set block='parablock' where block='textsection';
-update bolt_field_value set block='dualparablock' where block='text2colsection';
-update bolt_field_value set block='imageblock' where block='imagesection';
-update bolt_field_value set block='videoblock' where block='videosection';
